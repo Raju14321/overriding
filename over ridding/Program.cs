4039edddfe945a208f4655bed6374b0539b35cea
Raju14321 <100340131+Raju14321@users.noreply.github.com>
2022-03-01 09:44:03 +0530

    Add files via upload

---
+++ over ridding/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace over_ridding
+{
+    class Program
+    {
+        public void Show()
+        {
+            Console.WriteLine("This is a parent method");
+
+        }
+        public virtual void Test()      //overridable
+        {
+            Console.WriteLine("This is a also parent");
+        }
+
+    }
+    class Child: Program
+    {
+        public void Show(int i)
+        {
+            Console.WriteLine("Raju was small kid");
+        }
+        public override void Test()     //overriding
+        {
+            Console.WriteLine("My name is lucky");
+        }
+        static void Main()
+        {
+            Child c = new Child();
+            c.Show();
+            c.Test();
+            c.Show(12);
+
+            Console.ReadLine();
+        }
+    }
+
+}
